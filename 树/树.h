f3ce88e5e686d15a395c090c88d7f6f7b02989a3
zhangoneone <1193210854@qq.com>
2019-12-31 17:08:33 +0800

    avl树实现

--- 树/树.h
+++ 树/树.h
@@ -1,5 +1,12 @@
 #pragma once
 #include"common.h"
+
+
+#define ShowFun(fmt,...) printf(fmt,__VA_ARGS__);
+#define THR_TREE	1
+#define AVL_TREE	1
+#define HUFFMAN_TREE	1
+
 typedef struct TreeNode {
 	bool ltag;
 	struct TreeNode *lchild;
@@ -7,8 +14,7 @@ typedef struct TreeNode {
 	struct TreeNode *rchild;
 	bool rtag;
 	int node_cost;//哈夫曼树要使用的结点代价值
+	int balance_fact;//avl树平衡因子
 }TreeNode, *Tree;//线索二叉树的声明，兼容一般二叉树
 
 typedef int(*visit_t)(TreeNode* node, TreeNode* OutNode, void* args);
-
-#define ShowFun(fmt,...) printf(fmt,__VA_ARGS__);
